7e511519520b2fef81d8bd8671541b101feccbf6
rayx <anonymous@anonymous.com>
2026-09-14 15:22:05 +0800

    add input

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,6 @@
-import { faShareFromSquare, faEllipsis } from '@fortawesome/free-solid-svg-icons'
+import { faShareFromSquare, faEllipsis, faCopy, faArrowUpFromBracket, 
+         faPencil, faThumbsUp, faThumbsDown, faRepeat, faPlus, faBrain, 
+         faMicrophoneLines, faArrowRight} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Main.scss'
 
@@ -18,17 +20,43 @@ const Main = () => {
       </div>
       <div className='chat-content'>
         <div className="user-chat">
-          <div className='user-chat-box'>
-            Hello Catgirl
+          <div className='user-chat-container'>
+            <div className='user-chat-box'>
+              Hello Catgirl
+            </div>
+            <div className='user-chat-options'>
+              <button><FontAwesomeIcon icon={faCopy} /></button>
+              <button><FontAwesomeIcon icon={faArrowUpFromBracket} /></button>
+              <button><FontAwesomeIcon icon={faPencil} /></button>
+            </div>
           </div>
         </div>
         <div className="ai-chat">
           <div className='ai-chat-box'>
             Nyaa! What's up how are you?
           </div>
+          <div className='ai-chat-options'>
+            <button><FontAwesomeIcon icon={faCopy} /></button>
+            <button><FontAwesomeIcon icon={faThumbsUp} /></button>
+            <button><FontAwesomeIcon icon={faThumbsDown} /></button>
+            <button><FontAwesomeIcon icon={faArrowUpFromBracket} /></button>
+            <button><FontAwesomeIcon icon={faRepeat} /></button>
+            <button><FontAwesomeIcon icon={faEllipsis} /></button>
+          </div>
         </div>
       </div>
-      <div className='chat-input'></div>
+      <div className='chat-input'>
+        <div className="input-box">
+          <FontAwesomeIcon className="input-icons" icon={faPlus} />
+          <input />
+          <span className='thinking-mode'>
+            <span><FontAwesomeIcon icon={faBrain} /></span>
+            <span>Think</span>
+          </span>
+          <FontAwesomeIcon className="input-icons" icon={faMicrophoneLines} />
+          <button><FontAwesomeIcon icon={faArrowRight} /></button>
+        </div>
+      </div>
     </div>
   )
 }
