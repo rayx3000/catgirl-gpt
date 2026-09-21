1a3eb1e8132071a3e86c390a639c3e6ee08fc1ff
rayx <anonymous@anonymous.com>
2026-09-21 15:49:01 +0800

    put add input options

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,35 @@
 import { faShareFromSquare, faEllipsis, faCopy, faArrowUpFromBracket, 
          faPencil, faThumbsUp, faThumbsDown, faRepeat, faPlus, faBrain, 
-         faMicrophoneLines, faArrowRight} from '@fortawesome/free-solid-svg-icons'
+         faMicrophoneLines, faArrowRight, faPaperclip, faFolderPlus,
+        faImage, faMusic, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState, useEffect, useRef } from 'react';
 import './Main.scss'
 
-const Main = () => {
+const Main: React.FC = () => {
+
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        isVisible && 
+        menuRef.current && 
+        !menuRef.current.contains(event.target as Node)
+      ) {
+        setIsVisible(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isVisible]); 
+
+
   return (
     <div className='main'>
       <div className='chat-options'>
@@ -126,15 +151,46 @@ const Main = () => {
         </div>
       </div>
       <div className='chat-input'>
-        <div className="input-box">
-          <FontAwesomeIcon className="input-icons" icon={faPlus} />
-          <input />
-          <span className='thinking-mode'>
-            <span><FontAwesomeIcon icon={faBrain} /></span>
-            <span>Think</span>
-          </span>
-          <FontAwesomeIcon className="input-icons" icon={faMicrophoneLines} />
-          <button><FontAwesomeIcon icon={faArrowRight} /></button>
+        <div ref={menuRef}>
+          {isVisible && (
+            <div className='add-input-options'>
+              <button>
+                <FontAwesomeIcon icon={faPaperclip} />
+                <span>Add Photos and Files</span>
+              </button>
+              <button>
+                <FontAwesomeIcon icon={faFolderPlus} />
+                <span>Add from Library</span>
+              </button>
+              <button>
+                <FontAwesomeIcon icon={faImage} />
+                <span>Create Image</span>
+              </button>
+              <button>
+                <FontAwesomeIcon icon={faMusic} />
+                <span>Create Music</span>
+              </button>
+              <button>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                <span>Web Search</span>
+              </button>
+            </div>
+          )}
+
+          <div className="input-box">
+            <FontAwesomeIcon 
+              onClick={() => setIsVisible((prev) => !prev)} 
+              className="input-icons toggle-btn" 
+              icon={faPlus} 
+            />
+            <input />
+            <span className='thinking-mode'>
+              <span><FontAwesomeIcon icon={faBrain} /></span>
+              <span>Think</span>
+            </span>
+            <FontAwesomeIcon className="input-icons" icon={faMicrophoneLines} />
+            <button><FontAwesomeIcon icon={faArrowRight} /></button>
+          </div>
         </div>
       </div>
     </div>
